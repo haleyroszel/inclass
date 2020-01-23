482cf2b3232109ff63c15608e6a4b9e3814cbd9e
haleyroszel <roszel@ou.edu>
2020-01-23 13:10:30 -0600

    Add project files.

---
+++ class2dictionary/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class2dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<int, string> students = new Dictionary<int, string>();
+
+            students.Add(1, "Katie Harris");
+            students.Add(2, "Adam Ackerman");
+            students.Add(3, "Harry Smith");
+
+
+
+            if (students.ContainsKey(1))
+            {
+                students[1] = "Some new value";
+            }
+            else
+            {
+                students.Add(1, "Some Value");
+            }
+
+            foreach (var key in students.Keys)
+            {
+                Console.WriteLine($"{students[key]} {key}");
+            }
+
+
+            
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ inclass/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] studentIds = new int[3];
+            string[] studentFirstNames = new string[3];
+            string[] studentLastNames = { "Harris", "Ackerman", "Johnson" };
+            studentIds[0] = 1;
+            studentIds[1] = 2;
+            studentIds[2] = 3;
+            studentFirstNames[0] = "Katie";
+            studentFirstNames[1] = "Adam";
+            studentFirstNames[2] = "Harry";
+
+            for (int i = 0; i < studentIds.Length; i++)
+            {
+                Console.WriteLine($"{studentLastNames[i]}, {studentFirstNames[i]}, {studentIds[i]}");
+            }
+
+            List<Student> studs = new List<Student>();
+
+            //new class instance
+
+            Student katie = new Student();
+            katie.FirstName = "Katie";
+            katie.LastName = "Harris";
+            katie.ID = 1;
+            katie.Birthdate = Convert.ToDateTime("3/11/1998");
+
+            studs.Add(katie);
+
+            for (int i = 0; i < studentIds.Length; i++)
+            {
+                Student s = new Student();
+                s.FirstName = studentFirstNames[i];
+                s.LastName = studentLastNames[i];
+                s.ID = studentIds[i];
+
+                studs.Add(s);
+            }
+
+            foreach (var student in studs)
+            {
+                Console.WriteLine($"{student.LastName}, {student.FirstName}, {student.ID} was born on {student.Birthdate.ToLongDateString()} ");
+            }
+
+            foreach (var letter in student)
+            {
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
